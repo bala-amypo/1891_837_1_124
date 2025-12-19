a8aa6dd813ceec1e2010d60418b09e47ec18dad6
bala-amypo <bala@amypo.in>
2025-12-19 07:57:53 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+public void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException {
 
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Supplier Diversity Tracker is running");
-    }
+    resp.setContentType("text/plain");
+    resp.getWriter().write("Supplier Diversity Tracker is running");
 }
+
